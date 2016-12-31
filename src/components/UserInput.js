913848ea06b8b04b730c6d132f6fe1ff418b1dc2
H3mann <mann.harp@gmail.com>
2016-12-31 03:17:34 -0500

    bootstrap styling to userInput

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,44 +1,77 @@
 import React from 'react'
+import { Button, FormGroup, ControlLabel, FormControl, Form, HelpBlock, Col} from 'react-bootstrap';
 
 const UserInput = (props) => (
 
 <div>
-	<h1> Input Your Info Here </h1>
 
+	<Form horizontal>
+		<FormGroup>
+				<Col componentClass={ControlLabel} sm={1}>
+				Car Model
+				</Col>
+			<Col sm={3}>
+				<FormControl
+					className='formInput'
+					placeholder='car'
+					type='text'
+					onChange={props.handleCarChange}
+					value={props.car} />
+				<FormControl.Feedback />
+			</Col>
+		</FormGroup>
 
-<form onSubmit={props.handleSubmit}>
-<div className='form-group'>
-<input className='formInput'
-placeholder='car'
-type='text'
-onChange={props.handleCarChange}
-value={props.car} />
+		<FormGroup>
+				<Col componentClass={ControlLabel} sm={1}>
+					Colour
+				</Col>
+			<Col sm={3}>
+			<FormControl
+				className='formInput'
+				placeholder='colour'
+				type='text'
+				onChange={props.handleColourChange}
+				value={props.colour} />
+			<FormControl.Feedback />
+			</Col>
+		</FormGroup>
 
-<input className='formInput'
-placeholder='colour'
-type='text'
-onChange={props.handleColourChange}
-value={props.colour} />
+		<FormGroup>
+				<Col componentClass={ControlLabel} sm={1}>
+					Location
+				</Col>
+			<Col sm={3}>
+			<FormControl
+				className='formInput'
+				placeholder='location'
+				type='text'
+				onChange={props.handleLocationChange}
+				value={props.location} />
+			<FormControl.Feedback />
+			</Col>
+		</FormGroup>
 
-<input className='formInput'
-placeholder='location'
-type='text'
-onChange={props.handleLocationChange}
-value={props.location} />
-
-<input className='formInput'
-placeholder='time'
-type='text'
-onChange={props.handleTimeChange}
-value={props.time} />
-
-<button className='submitBtn' type='submit' onClick={props.handleSubmit}>SUBMIT</button>
-
-</div>
-
-</form>
-
-
+		<FormGroup>
+				<Col componentClass={ControlLabel} sm={1}>
+					Time
+				</Col>
+			<Col sm={3}>
+			<FormControl
+				className='formInput'
+				placeholder='time'
+				type='text'
+				onChange={props.handleTimeChange}
+				value={props.time} />
+			</Col>
+		</FormGroup>
+			
+			<FormGroup>
+				<Col smOffset={1} xs={3}>
+					<Button className='submitBtn' type='submit' onClick={props.handleSubmit}>
+					Submit</Button>
+				</Col>
+			</FormGroup>
+	</Form>
 
 </div>
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css'
 import {Provider} from 'react-redux'
 import configureStore from './store/configure-store'
 import routes from './config/routes'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css'
 
 
 render(routes, document.getElementById('root'))
